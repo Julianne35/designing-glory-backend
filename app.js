c407e60324105f52ac6792a841ac10255b65b9d3
Julianne35 <jbosko28@gmail.com>
2021-10-28 22:23:05 -0400

    edits to app.js to conn to secure server

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
+const cors = require("cors");
 const mongoose = require("mongoose");
-
+const express = require("express");
+const logger = require("morgan");
 require("./models/Email");
 const routes = require("./routes/routes");
 
-// const app = require("./app");
-
-const express = require("express");
-const cors = require("cors");
-const logger = require("morgan");
-
 const app = express();
 
+//---------use for node over secure server - add this when you start aws instance 
+// const https = require("https"),
+//     fs = require("fs");
+
+// const sslServer =  https.createServer (
+// {
+//   key: fs.readFileSync('/var/www/cert/privkey.pem'),
+//   cert: fs.readFileSync('/var/www/cert/fullchain.pem'),
+// },
+// app
+// )
+//--------end code block
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
+// Express only serves static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 app.use(express.json());
 app.use(logger("dev"));
 app.use("/", routes);
-
 app.use("/search", express.static("search"));
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 const mongoURI =
   "mongodb+srv://Julianne:Shareverse1234@svcluster.hlmba.mongodb.net/svs_data?retryWrites=true&w=majority";
@@ -38,6 +57,10 @@ conn.once("open", () => {
 
 conn.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-const server = app.listen(5005, () => {
-  console.log(`Express running PORT ${server.address().port}`);
+
+const server = app.listen(3001, (sslServer) => {console.log(`Express running PORT ${server.address().port}`)
 });
+
+// --------use for aws instance only (connected to 11-20)
+// sslServer.listen(3001, () => { console.log(`working!`) });
+//---------end code block
